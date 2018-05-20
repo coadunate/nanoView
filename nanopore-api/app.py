13ac790d5688afc17bae351c4264a8ff2d44b3f5
tayabsoomro <isoomro.t@gmail.com>
2018-05-20 08:46:02 -0600

    line plot up and ready for view

--- nanopore-api/app.py
+++ nanopore-api/app.py
@@ -7,6 +7,8 @@ from os.path import isfile, join
 import pandas as pd
 from operator import itemgetter
 from SAMParse import *
+from extract_tombo import Fast5Events
+import numpy as np
 # import pysam
 
 app = Flask(__name__)
@@ -38,7 +40,7 @@ def generatejson(name, directory, ref, sam):
 			path = "./data/"+name+"/"+base+".record"
 			# sys.stdout = open(path, 'w')
 			f = Fast5Event(directory+"/"+i)
-			record, max_min,a,b = f.get_list_event_data(file_path=sam)
+			record, max_min,a, pos = f.get_list_event_data(file_path=sam)
 			for i in range(len(record)):
 				if i % 2 == 1 or i == 0:
 					continue
@@ -48,13 +50,39 @@ def generatejson(name, directory, ref, sam):
 					for id in range(leng):
 						record[i][id][0] = float(record[i-1]) + float(diff)*float(id)
 			record.append(max_min)
+
+			d_counter = 0
+			# for i in range(len(a)):
+			# 	if a[i][1] == 'i':
+			# 		for item in record[(2*a[i][0])-(d_counter)]:
+			# 			item[-1] = 'i'
+			# 		# record[(2*a[i][0])-(d_counter)][:][-1] = 'i'
+			# 	elif a[i][1] == 'd':
+			# 		d_counter += 1
+			# 		for item in record[(2*a[i][0])-(d_counter)]:
+			# 			item[-1] = 'd'
+					# record[(2*a[i][0])-(d_counter)][:][-1] = 'd'
 			# print(record[0])
-			print(a)
-			print(b)
+			# for item in a:
+			# 	for i in range(len(record[(2 * (item - pos)) + 2])):
+			# 		record[(2 * (item - pos)) + 2][i][0][-1] = 'd'
+			# for item in b:
+			# 	for i in range(len(record[(2 * (item - pos)) + 2])):
+			# 		record[(2 * (item - pos)) + 2][i][0][-1] = 'i'
+			# for item in a:
+			# 	print(record[(2*item)+ 2 ])
+			# print(a)
+			# print(b)
+			# for item in record
+			# print(record)
 			with open(path, 'wb') as outfile:
 				pickle.dump(record, outfile)
 				pickle.dump(max_min, outfile)
-	return "Project Successfully Created", 201
+	# return "Project Successfully Created", 201
+	# for i in range(len(record)):
+	# 	for item in record[2*i]:
+	# 		print(item)
+
 	return json.dumps(record), 201
 
 
@@ -105,6 +133,176 @@ def dataselect():
 	for i in range(len(sub)):
 		projects[i] = sub[i]
 	return json.dumps(projects), 200
+
+
+@app.route('/tombo/<string:name>&<path:directory>&<path:sam>', methods = ['GET'])
+# @cross_origin()
+def tombo(name, directory, sam):
+	name = name[5:]
+	directory = directory[10:]
+	sam = sam[4:]
+	dir_path = "./data/"+name
+	if not os.path.exists(dir_path):
+		os.makedirs(dir_path)
+	# else:
+	# 	return 'Project Already Exists', 406
+
+	for i in listdir(directory):
+		if isfile(join(directory, i)):
+			base = i.split('.fast')[0]
+			path = "./data/"+name+"/"+base+".tombo.record"
+			# sys.stdout = open(path, 'w')
+			f = Fast5Events(directory+"/"+i)
+			record, max_min,a, pos = f.get_list_event_data(file_path=sam)
+			# for i in range(len(record)):
+			# 	if i % 2 == 1 or i == 0:
+			# 		continue
+			# 	else:
+			# 		leng = len(record[i])
+			# 		diff = 1.0/float(leng)
+			# 		for id in range(leng):
+			# 			record[i][id][0] = float(record[i-1]) + float(diff)*float(id)
+			# for item in a:
+			# 	record[item][-1] = 'd'
+			# for item in b:
+			# 	record[item][-1] = 'i'
+			d_counter = 0
+			for i in range(len(a)):
+				if a[i][1] == 'i':
+					record[1][((a[i][0]-(d_counter+1)) * 2) - 1][0][-1] = 'i'
+				elif a[i][1] == 'd':
+					d_counter += 1
+					record[1][((a[i][0]-(d_counter+1)) * 2) - 1][0][-1] = 'd'
+
+
+			record.append(max_min)
+			with open(path, 'wb') as outfile:
+				pickle.dump(record, outfile)
+				pickle.dump(max_min, outfile)
+	# return "Project Successfully Created", 201
+	return json.dumps(record), 201
+
+
+@app.route('/getall/<string:name>&<string:data>', methods = ['GET'])
+# @cross_origin()
+def getall(name, data):
+	mami = []
+	data = data[-1]
+	name = name[5:]
+	started = 0
+	directory = "./data/"+name
+	final_record = "["
+	first = 0
+	if data == '1':
+		for i in listdir(directory):
+			if isfile(join(directory, i)):
+				if i.endswith(".tombo.record"):
+					with open(directory+"/"+i, 'rb') as infile:
+						record = pickle.load(infile)
+					mami.append(record[-1])
+					final_record +=   json.dumps(record[0]) + ","
+					final_record += json.dumps(record[1:-1]) + ","
+					# final_record += json.dumps(record) + ","
+					# final_record += json.dumps(record[-1]) + ","
+	elif data == '2':
+		for i in listdir(directory):
+			if isfile(join(directory, i)):
+				if i.endswith(".normed.record"):
+					with open(directory+"/"+i, 'rb') as infile:
+						record = pickle.load(infile)
+					final_record += json.dumps(record[0]) + ","
+					final_record += json.dumps(record[1:])
+	else:
+		for i in listdir(directory):
+			if isfile(join(directory, i)):
+				if not i.endswith(".tombo.record"):
+					with open(directory+"/"+i, 'rb') as infile:
+						record = pickle.load(infile)
+					mami.append(record[-1])
+					final_record +=   json.dumps(record[0]) + ","
+					final_record += json.dumps(record[1:-1]) + ","
+				# final_record += json.dumps(record[-1]) + ","
+	try:
+		max_mami = max(mami,key=itemgetter(1))[0]
+		min_mami = min(mami,key=itemgetter(1))[1]
+		final_record += "["+max_mami+", "+min_mami+"]]"
+	except :
+		pass
+	else:
+		final_record += "]"
+	# final_record = final_record[:-1]
+	# final_record+= "]"
+	return final_record, 201
+
+
+@app.route('/normed/<string:name>&<path:directory>&<path:sam>', methods = ['GET'])
+def normed(name, directory, sam):
+	name = name[5:]
+	directory = directory[10:]
+	sam = sam[4:]
+	dir_path = "./data/"+name
+	if not os.path.exists(dir_path):
+		os.makedirs(dir_path)
+	for i in listdir(directory):
+		if isfile(join(directory, i)):
+			base = i.split('.fast')[0]
+			path = "./data/"+name+"/"+base+".normed.record"
+			# sys.stdout = open(path, 'w')
+			f = Fast5Events(directory+"/"+i)
+			# record, max_min,a, pos = f.get_list_event_data(file_path=sam)
+			shift0, scale0, bprime0, b_a0, record = get_data(directory+'/'+i)
+
+			# record.append(max_min)
+			with open(path, 'wb') as outfile:
+				pickle.dump(record, outfile)
+				# pickle.dump(max_min, outfile)
+	return json.dumps(record), 201
+
+def get_data(file):
+    f = h5py.File(file, 'r')
+    read = file.split('_read')[1]
+    read = read.split('_')[0]
+    a = f['Raw/Reads/Read_'+read+'/Signal']
+    b = []
+    for item in a:
+        b.append(item)
+    digi = f['UniqueGlobalKey/channel_id'].attrs['digitisation']
+    parange = f['UniqueGlobalKey/channel_id'].attrs['range']
+    offset = f['UniqueGlobalKey/channel_id'].attrs['offset']
+
+    events = f['/Analyses/RawGenomeCorrected_000/BaseCalled_template/Events']
+    start = []
+    for item in events:
+        start.append((item['start'], item['length']))
+
+    shift = np.median(b)
+    scale = np.median(np.abs(b - shift))
+    bprime = (b - shift)
+    b_a = (b - shift) / scale
+
+    record = get_list_values(start, b_a, file)
+
+    return shift, scale, bprime, b_a, record
+
+
+def get_list_values(start, b_a, file):
+    record = []
+    f = Fast5Events(file)
+    qname = f.get_qname()
+    record.append(qname)
+    counter = 0
+    len_counter = 0
+    sub0 = []
+    for i in range(len(start)):
+        length = start[i][1]
+        sub0.append(i)
+        sub1 = []
+        for a in range(length):
+            sub1.append(b_a[a + start[i][0]])
+        sub0.append(sub1)
+    record.append(sub0)
+    # print(record)
+    return record
 
 
 @app.after_request
---
+++ nanopore-api/extract_tombo.py
@@ -0,0 +1,349 @@
+""" Basic Program for extracing time and signal values for given event ranges.
+"""
+
+import h5py # for File
+import os
+from ont_fast5_api.fast5_info import Fast5Info # for Fast5Info
+import re # regular expressions
+import json # to parse event data
+from SAMParse import *
+import pandas as pd
+
+
+class Fast5Events(object):
+    """ This object is comprised of functions that can be performed on the event
+        data of the given fast5 file.
+    """
+
+    def __init__(self,filename):
+        """ Constructor. Opens the specified file
+            :param filename: Filename to open
+        """
+
+        # Attributes Initializeation
+
+        self.filename = filename
+        self.handle = None
+        self._is_open = False
+        self.status = Fast5Info(self.filename)
+        self.events = None
+
+
+        if self.status.valid:
+            self.handle = h5py.File(self.filename, 'r') # Open the file in read-only mode
+            self._is_open = True
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self,exception_type,exception_value,traceback):
+        self.close()
+        return False
+
+
+    def assert_open(self):
+        if not self._is_open:
+            raise IOError("Fast5 file is not open: {}".format(self.filename))
+
+    def close(self):
+        """ Closes the fast5 file that was opened by the object intitialization
+        """
+
+        if self._is_open:
+            if self.handle:
+                self.handle.close()
+                self.handle = None
+            self.filename = None
+            self._is_open = False
+            self.status = None
+
+    def populate_events(self):
+        """ Intelligently identifies the URL (within fast5 file) for Events dataset
+            and extracts the events and saves it into the events field of this
+            object
+        """
+        self.assert_open() # Throw an error if the fast5 file is not open
+
+        # Determining the Events dataset
+        all_objs = []  # Create a list of empty objects
+        self.handle.visit(all_objs.append) # populate all_objs with the objecs in the fast5 file
+        all_datasets = [ obj for obj in all_objs if isinstance(self.handle[obj],h5py.Dataset) ]
+        # dataset_name = all_datasets[ [ i for i, dataset in enumerate(all_datasets) if dataset.endswith('Events') ][0] ] # events dataset
+        dataset_name = '/Analyses/RawGenomeCorrected_000/BaseCalled_template/Events'
+        self.events = self.handle[dataset_name] # get the events array from the events dataset
+
+    def assert_events(self):
+        self.populate_events()
+        if self.events == None:
+            raise IOError("The Event data isn't populated yet")
+
+    def get_event_count(self):
+        """ Returns an integer value representing the number of events
+            that are present in the given fast5 file.
+        """
+        self.assert_events() # Throw an erorr if the event data isn't populated.
+
+        return len(self.events);
+
+
+    def get_event_time_data(self,event_start,event_end):
+        """ Returns a list containing the event time values of the events from
+            the given ranges of start and end events in the arguments
+        """
+        self.assert_events() # Throw an error if the event data isn't populated
+
+        time_data = [] # Create empty list which will contain the time data
+
+        time_index = self.get_start_time_index()
+
+        for i in range(event_start,event_end):
+            time_data.append(self.events[i][time_index])
+
+        return time_data
+
+    def get_event_signal_data(self,event_start,event_end):
+        """ Returns a list containing the event signal values of the events from
+            the given ranges of start and end events in the arguments
+        """
+        self.assert_events() # Throw an error if the event data isn't populated
+
+        signal_data = [] # Create empty list which will contain the signal data
+
+        signal_index = self.get_mean_signal_index()
+
+        for i in range(event_start,event_end):
+            signal_data.append(self.events[i][signal_index])
+
+        return signal_data
+
+
+    def get_set_event_signal_data(self,eventNumbers):
+        """ Returns a list containing the event signal values of the events from
+            the event numbers provided as arguments to this function
+        """
+        self.assert_events() # Throw an error if the event data isn't populated.
+
+        signal_data = [] # Create empty list which will contain the signal data
+
+        signal_index = self.get_mean_signal_index()
+
+        for i in range(len(eventNumbers)):
+            signal_data.append(self.events[eventNumbers[i]][signal_index])
+
+        return signal_data
+
+
+    def map_bp_to_events(self,bp):
+        """ Returns a list containing event numbers of the events that correspond
+            to the given list of 5'mers as an argument
+        """
+        self.assert_events() # Throw an error if the event data isn't populated.
+
+        if not bp:
+            raise IOError("The list of basepairs provided is not compatible.")
+
+
+        model_index = self.get_model_state_index()
+
+        bp_upperbound = len(bp) - 5
+        retEventNumbers = [] # empty list that will contain the event numbers
+        event_start = 0
+        matches_so_far = 0
+        for i in range(0,bp_upperbound+1): # loop through the base pairs
+            if matches_so_far >= 2:
+                break;
+            while(event_start < len(self.events)):
+                strbp = ''.join(bp[i:i+5])
+                strevent = "".join( [ chr(item) for item in self.events[event_start][model_index] ])
+                if(strbp == strevent):
+                    retEventNumbers.append(event_start)
+                    event_start = event_start + 1
+                    matches_so_far = matches_so_far + 1
+                    break
+                event_start = event_start + 1
+
+        if matches_so_far >= 2:
+            while(event_start < len(self.events)):
+
+                strbp = ''.join(bp[-5:])
+                strevent = ''.join( [ chr(item) for item in self.events[event_start][model_index] ])
+                if(strbp == strevent):
+                    retEventNumbers.append(event_start)
+                    break
+                else:
+                    event_start = event_start + 1
+        return retEventNumbers
+
+
+    def get_start_time_index(self):
+        """ Returns an integer value representing the offset of the start time
+            value of the event pore model
+        """
+        self.assert_events() # Throw an error if the event data isn't populated
+
+        # The field header array containing names of each offest of pore model
+        field_header_arr = self.events.dtype.names
+
+        # Getting the index of start time value
+        start_index = -1
+        for index,field in enumerate(field_header_arr):
+            if field == "start":
+                start_index = index
+
+        return start_index
+
+    def get_move_index(self):
+        """ Returns an integer value representing the move value
+        """
+
+        self.assert_events()
+
+        field_header_arr = self.events.dtype.names
+
+        move_index = -1
+        for index, field in enumerate(field_header_arr):
+            if field == "move":
+                move_index = index
+
+        return move_index
+
+    def get_model_state_index(self):
+        """ Returns an integer value representing the index of the model state
+            of the given fast5 file.
+        """
+        self.assert_events() # Throw an error if the event data isn't populated.
+
+        # The field header array containing names of each offest of pore model
+        field_header_arr = self.events.dtype.names
+
+        # Getting the index of model_state value
+        model_index = -1
+        for index,field in enumerate(field_header_arr):
+            if field == "model_state":
+                model_index = index
+        return model_index
+
+    def get_length_index(self):
+        """ Returns an integer value representing the index of the length
+            of the given fast5 file.
+        """
+        self.assert_events() # Throw an error if the event data isn't populated.
+
+        # The field header array containing names of each offest of pore model
+        field_header_arr = self.events.dtype.names
+
+        # Getting the index of length value
+        length_index = -1
+        for index,field in enumerate(field_header_arr):
+            if field == "length":
+                length_index = index
+        return length_index
+
+    def get_mean_signal_index(self):
+        """ Returns an integer value representing the offset of the mean signal
+            value of the event pore model
+        """
+        self.assert_events() # Throw an error if the event data isn't populated
+
+        # The field header array containing names of each offest of pore model
+        field_header_arr = self.events.dtype.names
+
+        # Getting the index of mean signal value
+        mean_signal = -1
+        for index,field in enumerate(field_header_arr):
+            if field == "norm_mean":
+                mean_signal = index
+        return mean_signal
+
+    def get_base_index(self):
+        """ Returns an integer value representing the offset of the mean signal
+            value of the event pore model
+        """
+        self.assert_events() # Throw an error if the event data isn't populated
+
+        # The field header array containing names of each offest of pore model
+        field_header_arr = self.events.dtype.names
+
+        # Getting the index of mean signal value
+        mean_signal = -1
+        for index,field in enumerate(field_header_arr):
+            if field == "base":
+                mean_signal = index
+        return mean_signal
+
+    def get_qname(self):
+        self.assert_events()
+
+        qname_path = '/Analyses/Basecall_1D_000/BaseCalled_template/Fastq'
+        # qname_path = '/UniqueGlobalKey/tracking_id'
+        dset = self.handle[qname_path]
+
+        # return dset.attrs['protocol_run_id']
+        return dset.value.decode("utf-8").split('\n')[0][1:].split('_')[-2]
+
+
+    def get_list_event_data(self,event_start = -1, event_end = -1, file_path=""):
+
+        signal_index = self.get_mean_signal_index()
+        time_index = self.get_start_time_index()
+        length_index = self.get_length_index()
+        base = self.get_base_index()
+
+        max_sig = 0
+        min_sig = 100
+
+        record = []
+        qname = self.get_qname()
+        # qname = qname.split('_')[-2]
+        pos = get_pos(file_path, qname)
+        # cigar_offsets_d = []
+        # cigar_offsets_i = []
+        cigar_offsets = []
+        cigar = get_cigar(file_path, qname)
+        for i in range(len(cigar)):
+            if cigar[i] == 'd':
+                cigar_offsets.append((i, 'd'))
+            if cigar[i] == 'i':
+                cigar_offsets.append((i, 'i'))
+        record.append(qname)
+
+        # record = "[\"" + str(self.get_qname()) + "\": "
+        sub0 = []
+        if event_start is -1 and event_end is -1:
+            prev_move = -1
+            # get all the events of a given fast5 file
+            #print("index,signal,time,model,length,stdv")
+            for i in range(0,self.get_event_count()):
+                if self.events[i][signal_index] > max_sig:
+                    max_sig = self.events[i][signal_index]
+                if self.events[i][signal_index] < min_sig:
+                    min_sig = self.events[i][signal_index]
+                        # if cigar[prev_move] !=
+                    # record.append(str("%06d" % prev_move))
+                # record.append(str(i+pos))
+                sub1 = []
+                sub0.append(str(pos + i))
+                sub1.append([str(i+pos), str(self.events[i][signal_index]),
+                    str(self.events[i][time_index]),
+                    str(self.events[i][length_index]), ""])
+                sub0.append(sub1)
+                # elif prev_move != -1 and self.events[i][move] == 0:
+                #     sub1.append([str(i+pos+1), str(self.events[i][signal_index]),
+                #         str(self.events[i][time_index]),
+                #         str(self.events[i][length_index])])
+                # elif prev_move != -1 and self.events[i][move] == 1:
+                #     prev_move += 1
+                #     record.append(sub1)
+                #     # record.append(str("%06d" % prev_move))
+                #     record.append(str(prev_move+pos))
+                #     sub1 = []
+                #     sub1.append([str(i+pos+1), str(self.events[i][signal_index]),
+                #         str(self.events[i][time_index]),
+                #         str(self.events[i][length_index])])
+            max_min = []
+            max_min.append(str(max_sig))
+            max_min.append(str(min_sig))
+            record.append(sub0)
+            # record.append(max_min)
+            return record, max_min, cigar_offsets, pos
